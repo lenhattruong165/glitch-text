c6aa37c7a6ba26d922a4f5c78b4fa46da13f129b
truong <trungli655@gmail.com>
2020-02-09 17:28:14 +0700

    First commit

---
+++ test.js
@@ -0,0 +1,8 @@
+const Glitter = require('discord-glitter');
+ 
+const glitter = new Glitter({
+    token: 'token',
+    //prefix: '@g', /* Custom prefix for bot commands. */
+});
+ 
+glitter.start();
